bbed30388907179cc9e376015757779ec8c78da1
mdevoe12 <matthew.devoe@hotelengine.com>
2020-12-16 15:47:28 -0700

    added private method for search_params

--- src/controllers/api/v1/search_controller.cr
+++ src/controllers/api/v1/search_controller.cr
@@ -2,6 +2,10 @@ require "../../../services/open_library_service.cr"
 
 class SearchController < ApplicationController
   def search
-    OpenLibraryService.get(author: params[:author])
+    OpenLibraryService.get(params: search_params)
+  end
+
+  private def search_params
+    { author: params[:author] }
   end
 end
